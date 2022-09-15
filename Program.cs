556951552617c01b4fdacfd55cfb41ce5255c60b
bkatona97 <48015452+bkatona97@users.noreply.github.com>
2022-09-15 09:09:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace demogarbagecollector
+{
+    class Program
+    {
+        public class Garbage 
+        {
+            public static int n = 0;
+            public Garbage()
+            {
+                n++;
+            }
+            ~Garbage()
+            {
+                n--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int last;
+            while (true)
+            {
+                last = Garbage.n;
+                Garbage trash = new Garbage();
+                if (last < Garbage.n)
+                    Console.WriteLine(Garbage.n);
+                else
+                    break;
+                
+            }
+            Console.WriteLine("Last: " + last);
+            Console.ReadKey();
+        }
+    }
+}
